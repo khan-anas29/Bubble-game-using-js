d6d7d00e95854064621de7d4e5b96eebdaefa5d4
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-11 22:47:33 +0530

    Added a javascript to created bubbles with random numbers

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+// to store bubbles
+var clutter="";
+
+for (var i=1 ;i<=180;i++){
+    // We can give i but we need random numbers of 1 digit
+    var num= Math.floor(Math.random()*10)
+    clutter = clutter + `<div class="bubble">${num}</div>`
+    // clutter +=`<div class="bubble">${num}</div>`
+}
+
+document.querySelector(".panel-bottom").innerHTML = clutter
