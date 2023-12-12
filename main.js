2351bccf6b3d0d38aacb9415e21e7947f1c0eccf
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-12 22:38:32 +0530

    Added timer using js

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-// to store bubbles
-var clutter="";
+function makeBubble() {
+  // to store bubbles
+  var clutter = "";
 
-for (var i=1 ;i<=180;i++){
+  for (var i = 1; i <= 180; i++) {
     // We can give i but we need random numbers of 1 digit
-    var num= Math.floor(Math.random()*10)
-    clutter = clutter + `<div class="bubble">${num}</div>`
+    var num = Math.floor(Math.random() * 10);
+    clutter = clutter + `<div class="bubble">${num}</div>`;
     // clutter +=`<div class="bubble">${num}</div>`
+  }
+
+  document.querySelector(".panel-bottom").innerHTML = clutter;
 }
 
-document.querySelector(".panel-bottom").innerHTML = clutter
+var timer= 60
+function runTimer() {
+    var timer_Interval = setInterval(()=>{
+        if (timer>0) {
+            timer--;
+            document.getElementById("timer").textContent=timer
+        }
+        else{
+            // to stop seInterval inorder to save memory space
+            clearInterval(timer_Interval)
+        }
+    },1000)
+
+    
+}
+runTimer()
+makeBubble()
